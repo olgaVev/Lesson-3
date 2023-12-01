f7ec1c98552639c2aa17897f9a3d5f0b4670ba85
olgaVev <olga.viushin@gmail.com>
2023-11-30 20:39:33 -0500

    modified:   Assesment3.js

--- Assesment3.js
+++ Assesment3.js
@@ -6,7 +6,7 @@ findGradeLevel("Sasha", "90");
 
 function findGradeLevel (name, mark){
 
-    if (mark>100094 || mark <0) {
+    if (mark>1000945 || mark <0) {
 
         
         console.log (name + " you got invalid mark:)")
